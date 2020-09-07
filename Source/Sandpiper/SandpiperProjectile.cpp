50e1d7374a075a4bd23ab838f9d036c8e059d7c5
Rahul Verma <rahulv@nvidia.com>
2020-09-07 09:14:07 +0530

    make span of bullets life as 200s

--- Source/Sandpiper/SandpiperProjectile.cpp
+++ Source/Sandpiper/SandpiperProjectile.cpp
@@ -28,7 +28,7 @@ ASandpiperProjectile::ASandpiperProjectile()
 	ProjectileMovement->bShouldBounce = false;
 
 	// Die after 3 seconds by default
-	InitialLifeSpan = 100.0f;
+	InitialLifeSpan = 200;
 }
 
 void ASandpiperProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
